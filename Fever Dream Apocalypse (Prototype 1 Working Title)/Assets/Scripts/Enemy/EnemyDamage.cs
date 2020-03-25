ed918c9a288ff312745e38d518a7ce541aca209d
Epiczd <nutball37@gmail.com>
2020-03-25 18:30:09 -0400

    Player Health/Enemy Damage

---
+++ Fever Dream Apocalypse (Prototype 1 Working Title)/Assets/Scripts/Enemy/EnemyDamage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    private float timeBtwAttack = 0f;
+
+    void OnCollisionStay(Collision collision)
+    {
+        while(collision.collider.tag == "Player" && timeBtwAttack == 0f)
+        {
+            timeBtwAttack++;
+            PlayerHealth player = collision.transform.GetComponent<PlayerHealth>();
+
+            player.TakeDamage(damage);
+            break;
+        }
+
+        if(timeBtwAttack <= 1f && timeBtwAttack > 0f)
+        {
+            timeBtwAttack -= Time.deltaTime * 2.5f;
+        }
+        else
+        {
+            timeBtwAttack = 0f;
+        }
+    }
+}
---
+++ Fever Dream Apocalypse (Prototype 1 Working Title)/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    //Amount of health the player has
+    [SerializeField] private float health;
+
+    //When the player takes damage, they lose health by the amount of damage
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        if(health <= 0f)
+        {
+            Death();
+        }
+    }
+
+    //When the player loses all health, it will die
+    void Death()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+}
