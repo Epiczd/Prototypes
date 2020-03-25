e406f6453fff59ce1c056bc797dbaafc53fb5995
Epiczd <nutball37@gmail.com>
2020-03-25 14:46:40 -0400

    Axe can now deal damage

--- Fever Dream Apocalypse (Prototype 1 Working Title)/Assets/Scripts/Combat/Weapons/Axe.cs
+++ Fever Dream Apocalypse (Prototype 1 Working Title)/Assets/Scripts/Combat/Weapons/Axe.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class Axe : MonoBehaviour
 {
+    //Sound of the axe swinging
     [SerializeField] private AudioSource axeSwing;
 
+    //Animator, that plays the axe animation
     [SerializeField] private Animator axeAnimator;
 
+    //Blade of the axe
+    [SerializeField] private Collider axeBlade;
+
+    //Amount of damage dealt by the weapon (for some reason, this deals double damage?)
+    [SerializeField] private float damage;
+
+    //Time in between each attack
     private float timeBtwAttack = 0f;
+
+    //On start, damage is cut in half
+    private void Start()
+    {
+        damage /= 2;
+    }
 
     // Update is called once per frame
     void Update()
     {
         SwingAxe();
 
+        //Time between attack will decrease, after each attack
         while (timeBtwAttack <= 1f)
         {
             timeBtwAttack -= Time.deltaTime * 2.5f;
@@ -23,6 +39,7 @@ public class Axe : MonoBehaviour
 
         if (timeBtwAttack <= 0f)
         {
+            axeBlade.enabled = false;
             timeBtwAttack = 0f;
             axeAnimator.SetBool("Attacking", false);
         }
@@ -31,11 +48,24 @@ public class Axe : MonoBehaviour
     //Lets the player swing the axe
     void SwingAxe()
     {
+        //If the player presses left mouse, they will swing the axe
         if (Input.GetButtonDown("Attack") && timeBtwAttack == 0f)
         {
+            axeBlade.enabled = true;
             axeSwing.Play();
             axeAnimator.SetBool("Attacking", true);
             timeBtwAttack++;
+        }
+    }
+
+    //If the axe hits an enemy, the enemy will take damage
+    void OnTriggerEnter(Collider collision)
+    {
+        if(collision.CompareTag("Enemy"))
+        {
+            EnemyHealth enemy = collision.transform.GetComponent<EnemyHealth>();
+
+            enemy.TakeDamage(damage);
         }
     }
 }
---
+++ Fever Dream Apocalypse (Prototype 1 Working Title)/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    //Amount of health the enemy has
+    [SerializeField] private float health;
+    
+    //When the enemy takes damage, this subtracts from its health
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+
+        if(health <= 0f)
+        {
+            EnemyDeath();
+        }
+    }
+
+    //When the enemy reaches zero health, it will die
+    void EnemyDeath()
+    {
+        Destroy(gameObject);
+    }
+}
