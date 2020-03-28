bc44caf127e304c83c8861f1153f7b8fb713d2e3
Epiczd <nutball37@gmail.com>
2020-03-27 21:46:16 -0400

    Deathplain

---
+++ Fever Dream Apocalypse (Prototype 1 Working Title)/Assets/Scripts/Environment/Deathplain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Deathplain : MonoBehaviour
+{
+    //If the player falls into the deathplain, the player go to the game over screen
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
