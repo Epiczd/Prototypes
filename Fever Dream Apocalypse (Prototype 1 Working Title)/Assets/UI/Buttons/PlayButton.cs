ee88107aeecd6d380477d9a5c01c5421ccf5e75d
Epiczd <nutball37@gmail.com>
2020-03-27 21:51:06 -0400

    Title Screen

---
+++ Fever Dream Apocalypse (Prototype 1 Working Title)/Assets/UI/Buttons/PlayButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+    //The PlayButton
+    [SerializeField] private Button play;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        play.onClick.AddListener(onClick);
+    }
+
+    //When the player clicks the playbutton, the first level will load
+    void onClick()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+}
