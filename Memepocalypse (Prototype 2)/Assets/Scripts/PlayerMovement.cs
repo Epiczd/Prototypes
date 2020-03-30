d952c4e7a735b3d5ac0241306902298e5163aed8
Epiczd <nutball37@gmail.com>
2020-03-30 15:03:24 -0400

    Player Movement

---
+++ Memepocalypse (Prototype 2)/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Speed the player moves
+    private float moveSpeed = 10f;
+
+    //Lets the player move
+    void Update()
+    {
+        Movement();
+    }
+
+    //Uses WSAD/Arrow Keys to move
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.right * Time.deltaTime * moveSpeed * x);
+        transform.Translate(Vector2.up * Time.deltaTime * moveSpeed * y);
+    }
+}
