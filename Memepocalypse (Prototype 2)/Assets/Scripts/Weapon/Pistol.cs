5165ec297a1d5710379be3aa691759c6dfb03645
Epiczd <nutball37@gmail.com>
2020-04-06 16:06:42 -0400

    Prototype 2 Project File

---
+++ Memepocalypse (Prototype 2)/Assets/Scripts/Weapon/Pistol.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : MonoBehaviour
+{
+    //The gun
+    [SerializeField] private Transform gun;
+
+    //The gun's measure
+    [SerializeField] private Camera gunCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
